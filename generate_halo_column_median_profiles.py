4b1eb5e2adeb4db359ffedf1f5c1fa5ad6c9d29e
ibutsky <iryna.butsky@gmail.com>
2019-01-31 15:22:02 -0800

    making separate analysis for median profiles

---
+++ generate_halo_column_median_profiles.py
@@ -0,0 +1,53 @@
+import numpy as np
+import h5py as h5
+import sys
+
+sys.path.append("/nobackup/ibutsky/scripts/plot_help/")
+import ion_plot_definitions as ipd
+
+
+def generate_ion_profiles(output, ion_list, nbins = 800, rmax = 300):
+    frb = h5.File('/nobackup/ibutsky/data/YalePaper/romulusC.%06d_combined_halo_ion_data2.h5'%(output), 'r')
+    plot_file = h5.File('/nobackup/ibutsky/data/YalePaper/romulusC.%06d_combined_halo_ion_profile_data3.h5'%(output), 'w')
+
+    bin_name_list = ['low_mass', 'med_mass', 'high_mass', 'dist_1', 'dist_2', 'dist_3', 'dist_4']
+    plot_names = ['x_median', 'y_median', 'y_err']
+
+    for i, ion in enumerate(ion_list):
+        ion_out = ion.replace(" ", "")
+        for bin_name in bin_name_list:
+            print(ion, bin_name)
+            r_arr = frb[('%s_rbins_%s'%(ion_out, bin_name))][:]
+            col_arr = frb[('%s_col_%s'%(ion_out, bin_name))][:]
+            
+
+            nbins_med = 60
+            rbins_med = np.linspace(0, rmax, nbins_med)
+            y_median, y_err = ipd.make_profiles(r, col, rbins_med, nbins_med)
+                
+            plot_data = [rbins_med, y_median, y_err]
+            for pdata, pname in zip(plot_data, plot_names):
+                dset = '%s_%s_%s'%(ion_out, bin_name, pname)
+                plot_file.create_dataset(dset, data = pdata)
+                plot_file.flush()
+
+                
+#### NEXT STEP: add covering fraction 
+ion_list = ['H I', 'O VI', 'Si II', 'Si III', 'Si IV', 'C II', 'C III', 'C IV']
+#ion_list = ['O VI']
+output = 3035
+generate_ion_histograms(output, ion_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
