749cb88c74b6c278d44e363316f4b1e6c573e6ab
Iryna Sotolongo Butsky <ibutsky@pfe20.nas.nasa.gov>
2019-01-31 14:23:17 -0800

    fixing typo bug

--- generate_halo_column_histograms.py
+++ generate_halo_column_histograms.py
@@ -22,7 +22,7 @@ def return_histogram_data(r_arr, cdens_arr, nbins = 800, rmax = 300):
 
 def generate_ion_histograms(output, ion_list, nbins = 800, rmax = 300):
     frb = h5.File('/nobackup/ibutsky/data/YalePaper/romulusC.%06d_combined_halo_ion_data2.h5'%(output), 'r')
-    plot_file = h5.File('/nobackup/ibutsky/data/YalePaper/romulusC.%06d_combined_halo_ion_plot_data.h5'%(output), 'a')
+    plot_file = h5.File('/nobackup/ibutsky/data/YalePaper/romulusC.%06d_combined_halo_ion_plot_data3.h5'%(output), 'w')
 
     bin_name_list = ['low_mass', 'med_mass', 'high_mass', 'dist_1', 'dist_2', 'dist_3', 'dist_4']
     plot_names = ['xbins', 'ybins', 'counts', 'x_median', 'y_median', 'y_err']
@@ -31,10 +31,9 @@ def generate_ion_histograms(output, ion_list, nbins = 800, rmax = 300):
         ion_out = ion.replace(" ", "")
         for bin_name in bin_name_list:
             print(ion, bin_name)
-            r_arr = frb[('%s_rbins_%s'%(ion_out, bin_name))]
-            col_arr = frb[('%s_col_%s'%(ion_out, bin_name))]
+            r_arr = frb[('%s_rbins_%s'%(ion_out, bin_name))][:]
+            col_arr = frb[('%s_col_%s'%(ion_out, bin_name))][:]
             
-
             # since the arrays are huge (~90 million entries), we're going to split them up
             # and calculate the counts separately (much faster) and then add them upp
             num_slices = 8
@@ -44,20 +43,21 @@ def generate_ion_histograms(output, ion_list, nbins = 800, rmax = 300):
 
             nbins_med = 60
             rbins_med = np.linspace(0, rmax, nbins_med)
-            median_list = np.zeros(len(rbins_med), num_slices)
-            err_list = np.zeros(len(rbins_med), num_slices)
+            median_list = np.zeros((len(rbins_med), num_slices))
+            err_list = np.zeros((len(rbins_med), num_slices))
             for m in range(len(multiplier)-1):
-                print(m)
                 start = multiplier[m] * dlen
                 end = multiplier[m+1] * dlen
                 r = r_arr[start:end]
-                col = r_arr[start:end]
-                xbins, ybins, dcounts = return_histogram_data(r, col)
-                counts += dcounts
+                col = col_arr[start:end]
 
+                xbins, ybins, dcounts = return_histogram_data(r, col, nbins=nbins, rmax=rmax)
+                counts += dcounts
+ 
                 y_median, y_err = ipd.make_profiles(r, col, rbins_med, nbins_med)
-                median_list[m] = y_median
-                err_list[m] = y_err
+                for ind in range(len(y_median)):
+                    median_list[ind][m] = y_median[ind]
+                    err_list[ind][m] = y_err[ind]
 
             print('made histogram!')
 
@@ -65,7 +65,7 @@ def generate_ion_histograms(output, ion_list, nbins = 800, rmax = 300):
             y_err = np.zeros(len(y_err))
             for j in range(len(y_median)):
                 y_median[j] = np.median(median_list[j])
-                y_err[j] = np.sqrt(err_list[m]**2)
+                y_err[j] = np.sqrt(np.sum(err_list[m]**2))
 
             plot_data = [xbins, ybins, counts, rbins_med, y_median, y_err]
             for pdata, pname in zip(plot_data, plot_names):
@@ -76,6 +76,7 @@ def generate_ion_histograms(output, ion_list, nbins = 800, rmax = 300):
                 
 #### NEXT STEP: add covering fraction 
 ion_list = ['H I', 'O VI', 'Si II', 'Si III', 'Si IV', 'C II', 'C III', 'C IV']
+#ion_list = ['O VI']
 output = 3035
 generate_ion_histograms(output, ion_list)
 
