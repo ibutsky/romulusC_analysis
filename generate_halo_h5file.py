44b469c0cb3f41b2af07ee9a4061eceece7f8bdf
Iryna Sotolongo Butsky <ibutsky@pfe20.nas.nasa.gov>
2019-04-02 10:40:03 -0700

    changing redshift of metallicity plot

--- generate_halo_h5file.py
+++ generate_halo_h5file.py
@@ -15,7 +15,7 @@ import os, sys
 import romulus_analysis_helper as rom_help
 import ion_plot_definitions as ion_help
 
-def generate_halo_column_data(sim, output, ion_list, res = 800):
+def generate_halo_column_data(sim, output, ion_list, res = 800, start_index = 0, end_index = 1000):
 
     field_list = ion_help.generate_ion_field_list(ion_list, 'number_density')
     ds = yt.load('/nobackupp2/ibutsky/simulations/%s/%s.%06d'%(sim, sim, output))
@@ -23,48 +23,43 @@ def generate_halo_column_data(sim, output, ion_list, res = 800):
 
     halo_props = h5.File('/nobackupp2/ibutsky/data/%s_halo_data_%i'%(sim, output), 'r')
     halo_ids = halo_props['halo_id'][:]
+    mstars = halo_props['mstar'][:]
+    contamination = halo_props['contamination'][:]
 
-
-    for i in range(len(halo_ids)):
-        halo_id = halo_ids[i]
-        rvir = halo_props['rvir'][i]
-        mstar = halo_props['mstar'][i]
-        halo_center = halo_props['center'][i]
-        center = (halo_center / ds.length_unit).d
-######################### TEMPORARY IF STATEMENT ######################
-        
-        if mstar > 1e9 and halo_id >0:
-            print(i, halo_id, mstar)
-            sys.stdout.flush()
-
-            cdens_file = h5.File('/nobackupp2/ibutsky/data/%s/column_%i_halo%i'%(sim, output, halo_id), 'a') 
+    mask = (mstars >= 1e9) & (halo_ids > 0) & (contamination < 0.05)
     
-            width = yt.YTQuantity(2*rvir, 'kpc')
-            px, py = np.mgrid[-width/2:width/2:res*1j, -width/2:width/2:res*1j]
-            radius = (px**2.0 + py**2.0)**0.5
+    for i in range(start_index, end_index):
+        halo_id = halo_ids[mask][i]
+        mstar = mstars[mask][i]
+        halo_center = halo_props['center'][mask][i]
+        center = (halo_center / ds.length_unit).d
 
-            if "px" not in cdens_file.keys():
-                cdens_file.create_dataset("px", data=px.ravel())
+        print(i, halo_id, mstar)
+        sys.stdout.flush()
 
-            if "py" not in cdens_file.keys():
-                cdens_file.create_dataset("py", data=py.ravel())
+        cdens_file = h5.File('/nobackupp2/ibutsky/data/%s/column_%i_halo%i'%(sim, output, halo_id), 'a') 
+    
+        width = yt.YTQuantity(300, 'kpc')
+        px, py = np.mgrid[-width/2:width/2:res*1j, -width/2:width/2:res*1j]
+        radius = (px**2.0 + py**2.0)**0.5
 
-            if "radius" not in cdens_file.keys():
-                cdens_file.create_dataset("radius", data = radius.ravel())
+        if "radius" not in cdens_file.keys():
+            cdens_file.create_dataset("radius", data = radius.ravel())
+            
+        for j, ion in enumerate(ion_list):
+            for axis in ['x']:
+                dset = "%s_%s" % (ion, axis)
+                if dset not in cdens_file.keys():
+                    frb = ion_help.make_projection(ds, axis, field_list, center, width, res = res)
+                    cdens_file.create_dataset(dset, data=frb[field_list[j]].ravel())
+                    cdens_file.flush()
 
 
-            for axis in ['x', 'y', 'z']:
-                for j, ion in enumerate(ion_list):
-                    dset = "%s_%s" % (ion, axis)
-                    if dset not in cdens_file.keys():
-                        frb = ion_help.make_projection(ds, axis, field_list, center, width, res = res)
-                        cdens_file.create_dataset(dset, data=frb[field_list[j]].ravel())
-                        cdens_file.flush()
 
-
-
-sim =  sys.argv[1]
-output = int(sys.argv[2])
+sim =  'romulusC'
+output = 3035
+start_index = int(sys.argv[1])
+end_index = int(sys.argv[2])
 ion_list = ['H I', 'O VI', 'Si II', 'Si III', 'Si IV', 'C II', 'C III', 'C IV']
 ion_list = ['H I', 'O VI', 'C IV']
 
